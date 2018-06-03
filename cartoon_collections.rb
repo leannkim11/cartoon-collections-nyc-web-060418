74495b4e48b750cd7270cd161a646f01547a95f8
Leann Kim <leannminakim@gmail.com>
2018-06-03 20:31:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,15 @@
 # def roll_call_dwarves(dwarf_arr)# code an argument here
 #   dwarf_arr.each_with_index{|index, dwarf| puts "#{index} #{dwarf}"
 # end
-# 
+#
 # def summon_captain_planet(cp_arr)# code an argument here
-#   
+#
 # end
-# 
+#
 # def long_planeteer_calls# code an argument here
 #   # Your code here
 # end
-# 
+#
 # def find_the_cheese# code an argument here
 #   # the array below is here to help
 #   cheese_types = ["cheddar", "gouda", "camembert"]
@@ -20,11 +20,11 @@ planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
 
 def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
+  i = 0
+  while i < array.length
     puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
+    i += 1
+  end
 end
 
 roll_call_dwarves(dwarves)
@@ -33,25 +33,25 @@ roll_call_dwarves(dwarves)
 
 def summon_captain_planet(array)
   nu_array = []
-  i = 0 
-  while i < array.length 
+  i = 0
+  while i < array.length
     nu_array << array[i].capitalize + "!"
-    i += 1 
-  end 
+    i += 1
+  end
   nu_array
 end
 
 summon_captain_planet(planeteer_calls)
 
 
-def long_planeteer_calls(array) 
-  i = 0 
+def long_planeteer_calls(array)
+  i = 0
   if  array.any? {|i| i.length > 4}
     return true
-  else 
+  else
     return false
   i = i + 1
-  end 
+  end
 end
 
 long_planeteer_calls(call_screams)
@@ -66,7 +66,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
     cheese_types.include?(type)
-  end 
+  end
 end
 
 find_the_cheese(snacks)
@@ -81,7 +81,7 @@ def find_the_cheese2(array, array2)
   cheese_types = array2
     array.find do |type|
     cheese_types.include?(type)
-  end 
+  end
 end
 
 find_the_cheese2(snacks, cheese_types)
